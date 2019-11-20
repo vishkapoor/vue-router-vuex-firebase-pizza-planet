2be1339020ab26b4ae5a1f3258ad2346c3262a38
Vishal Kapoor <vishal.kapoor@ecdltd.com>
2019-11-20 10:01:09 +0000

    done

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,66 +1,73 @@
 <template>
-<div class="row">
-    <div class="col-md-6 col-sm-12">
-
+<div>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <create-pizza></create-pizza>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <h3> Menu:</h3>
+            <table class="table table-hover">
+                <thead class="default">
+                    <tr>
+                        <th>Item(s)</th>
+                        <th>Remove From Menu</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th colspan="4">
+                            Order Number: 1
+                        </th>
+                    </tr>
+                    <tr>
+                        <td> Margheria </td>
+                        <td>
+                            <button class="btn btn-outline-danger btn-sm">x</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-    <div class="col-md-6 col-sm-12">
-        <h3> Menu:</h3>
-        <table class="table table-hover">
-            <thead class="default">
-                <tr>
-                    <th>Item(s)</th>
-                    <th>Remove From Menu</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td> Margheria </td>
-                    <td>
-                        <button class="btn btn-sm btn-outline-danger">
-                            x
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm-12 col-md-12">
-        <h3>Current Orders</h3>
-        <table class="table table-sm">
-            <thead class="default">
-                <tr>
-                    <th>Item</th>
-                    <th>Size</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th colspan="4">Order Number: 1
-                        <spam class="pull-right">
-                            <button class="btn btn-sm btn-outline-danger">x</button>
-                        </spam>
-                    </th>
-                </tr>
-                  <tr>
-                    <td> Margheria </td>
-                    <td>
-                        <button class="btn btn-sm btn-outline-danger">
-                            x
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <h3>Current Orders</h3>
+            <table class="table table-sm">
+                <thead class="default">
+                    <tr>
+                        <th>Item</th>
+                        <th>Size</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th colspan="4">Order Number: 1
+                            <spam class="pull-right">
+                                <button class="btn btn-sm btn-outline-danger">x</button>
+                            </spam>
+                        </th>
+                    </tr>
+                      <tr>
+                        <td> Margheria </td>
+                        <td>9"</td>
+                        <td>1</td>
+                        <td>6.95</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 </template>
 <script>
+import CreatePizza from './CreatePizza.vue';
 export default {
   name: 'Admin',
+  components: {
+    CreatePizza
+  },
   data() {
     return {
 
---
+++ src/components/CreatePizza.vue
@@ -0,0 +1,90 @@
+<template>
+<form>
+    <h3>Add New Pizza</h3>
+    <div class="form-group row">
+        <label class="col-sm-3">Name: </label>
+        <div class="col-sm-9">
+            <input
+                v-model="form.name"
+                type="text" class="form-control input-sm" />
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label class="col-sm-3">Description:</label>
+        <div class="col-sm-9">
+            <textarea
+                v-model="form.description"
+                rows="5" class="form-control input-sm"></textarea>
+        </div>
+    </div>
+
+    <p><strong>Option 1:</strong></p>
+    <div class="form-group row">
+        <label class="col-sm-3">Size ("):</label>
+        <div class="col-sm-9">
+            <input
+                v-model='form.options[0].size'
+                type="text" class="form-control input-sm" />
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label class="col-sm-3">Price:</label>
+        <div class="col-sm-9">
+            <input
+                v-model='form.options[0].price'
+                type="text" class="form-control input-sm" />
+        </div>
+    </div>
+
+    <p><strong>Option 2:</strong></p>
+    <div class="form-group row">
+        <label class="col-sm-3">Size ("):</label>
+        <div class="col-sm-9">
+            <input
+                 v-model='form.options[1].size'
+                type="text" class="form-control input-sm" />
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label class="col-sm-3">Price:</label>
+        <div class="col-sm-9">
+            <input
+                 v-model='form.options[1].price'
+                type="text" class="form-control input-sm" />
+        </div>
+    </div>
+
+    <div class="form-grouop row">
+        <button class="btn btn-success btn-block" type="button">
+            Add
+        </button>
+    </div>
+
+</form>
+</template>
+<script>
+export default {
+    name: 'CreatePizza',
+    data() {
+        return {
+            form: {
+                name: '',
+                description: '',
+                quantity: 1,
+                options: [
+                    { size: '', price: '' },
+                    { size: '', price: '' },
+                ],
+            }
+        }
+    },
+}
+</script>
+<style lang="css" scoped>
+.row {
+    margin-bottom: 15px;
+}
+</style>
