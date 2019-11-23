f726e22d42dabd07aac78a54850d6b1507e51b95
Vishal Kapoor <vishal.kapoor@ecdltd.com>
2019-11-23 10:54:45 +0000

    done

---
+++ src/routes.js
@@ -0,0 +1,54 @@
+import Home from './components/Home.vue';
+import Menu from './components/Menu.vue';
+import About from './components/About.vue';
+import History from './components/History.vue';
+import Contact from './components/Contact.vue';
+import Delivery from './components/Delivery.vue';
+import Admin from './components/Admin.vue';
+
+
+export const routes = [
+    {
+      path: '/',
+      component: Home,
+      name: 'home' ,
+    },
+    {
+      path: '/menu',
+      component: Menu,
+      name: 'menu'
+    },
+    {
+      path: '/admin',
+      component: Admin,
+      name: 'admin',
+      beforeEnter: (to, from, next) => {
+        alert('This area is restrictred, please login to continue...');
+        next();
+      },
+    },
+    {
+      path: '/about',
+      component: About,
+      name: 'about',
+      children: [{
+          path: '/contact',
+          component: Contact,
+          name: 'contact'
+        },
+        {
+          path: '/history',
+          component: History,
+          name: 'history'
+        },
+        {
+          path: '/delivery',
+          component: Delivery,
+          name: 'delivery'
+        }
+    ]},
+    {
+      path: '*',
+      redirect: '/'
+    }
+];
