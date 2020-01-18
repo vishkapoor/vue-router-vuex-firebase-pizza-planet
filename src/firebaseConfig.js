cd92a41d2e6bc8f01c76fc84f84a07c896c9f29c
Vishal Kapoor <vishal.kapoor@ecdltd.co.uk>
2020-01-18 01:07:00 +0000

    api keys protected

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,6 @@
 import Firebase from 'firebase';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBBN7ZdtXz6ZlWyvviOeD3xCzUw526Dwl4",
-    authDomain: "pizza-planet-5ca8f.firebaseapp.com",
-    databaseURL: "https://pizza-planet-5ca8f.firebaseio.com",
-    projectId: "pizza-planet-5ca8f",
-    storageBucket: "pizza-planet-5ca8f.appspot.com",
-    messagingSenderId: "153894857749",
-    appId: "1:153894857749:web:d29ac4f14e623e7c58b21c"
-};
+import {firebaseConfig} from  '../firebaseConfig.js';
 
 // Initialize Firebase
 const firebaseApp = Firebase.initializeApp(firebaseConfig);
